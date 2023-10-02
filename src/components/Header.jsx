9a796d51dae7010353f4e7dd874e514ced57dcc3
Elisha416 <elishamutungi@gmail.com>
2023-10-02 14:48:39 +0300

    header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+export default function Header() {
+  return (
+    <Header>
+    <div>
+      <h1>
+        <span>PEAK</span>
+        <span>ESTATE</span>
+      </h1>
+    </div>
+    </Header>
+  )
+}
